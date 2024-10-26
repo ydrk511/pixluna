92e2b045827315defa1c34b2530d83ae67c0c40f
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2024-10-26 11:19:07 +0800

    prettier

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,37 +2,37 @@ import { Context, Logger } from 'koishi'
 import type { Config } from '../config'
 
 export class PixlunaLogger {
-  private logger: Logger
-  private config: Config
+    private logger: Logger
+    private config: Config
 
-  constructor(ctx: Context, config: Config) {
-    this.logger = ctx.logger('pixluna')
-    this.config = config
-  }
-
-  error(message: string, ...args: any[]) {
-    this.logger.error(message, ...args)
-  }
-
-  success(message: string, ...args: any[]) {
-    this.logger.success(message, ...args)
-  }
-
-  warn(message: string, ...args: any[]) {
-    this.logger.warn(message, ...args)
-  }
-
-  info(message: string, ...args: any[]) {
-    this.logger.info(message, ...args)
-  }
-
-  debug(message: string, ...args: any[]) {
-    if (this.config.isLog) {
-      this.logger.debug(message, ...args)
+    constructor(ctx: Context, config: Config) {
+        this.logger = ctx.logger('pixluna')
+        this.config = config
     }
-  }
+
+    error(message: string, ...args: any[]) {
+        this.logger.error(message, ...args)
+    }
+
+    success(message: string, ...args: any[]) {
+        this.logger.success(message, ...args)
+    }
+
+    warn(message: string, ...args: any[]) {
+        this.logger.warn(message, ...args)
+    }
+
+    info(message: string, ...args: any[]) {
+        this.logger.info(message, ...args)
+    }
+
+    debug(message: string, ...args: any[]) {
+        if (this.config.isLog) {
+            this.logger.debug(message, ...args)
+        }
+    }
 }
 
 export function createLogger(ctx: Context, config: Config): PixlunaLogger {
-  return new PixlunaLogger(ctx, config)
+    return new PixlunaLogger(ctx, config)
 }
