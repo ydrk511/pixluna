66ccc2a0d87b57e9fe4dd55b9cce0e90862b6340
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2024-10-29 21:46:04 +0800

    feat: 使随机算法密码学安全

--- src/utils/shuffle.ts
+++ src/utils/shuffle.ts
@@ -1,8 +1,19 @@
+import { webcrypto } from 'crypto'
+
 export function shuffleArray<T>(array: T[]): T[] {
+    if (array.length <= 1) {
+        return [...array]
+    }
+
     const shuffled = [...array]
+
+    const randomBuffer = new Uint32Array(shuffled.length)
+    webcrypto.getRandomValues(randomBuffer)
+
     for (let i = shuffled.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1))
+        const j = randomBuffer[i] % (i + 1)
         ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
     }
+
     return shuffled
 }
